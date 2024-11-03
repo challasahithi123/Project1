bff8cab90a064aa10949cf1871e99a0b3e9e1e6f
SAHITHI <SAHITHI@LAPTOP-6AAHA2LJ>
2024-11-03 12:07:33 +0530

    commit message

---
+++ Project1/src/com/Programs/M2.java
@@ -0,0 +1,13 @@
+package com.Programs;
+
+public class M2 {
+
+	public static void main(String[] args) {
+		int a=50;
+		for(int i=1;i<=10;i++) {
+			System.out.println(a*i);
+		}
+
+	}
+
+}
---
+++ Project1/src/com/Programs/Printsumofnumbers.java
@@ -0,0 +1,16 @@
+package com.Programs;
+
+public class Printsumofnumbers {
+
+	public static void main(String[] args) {
+		int a=50;
+		int sum=0;
+		for(int i=0;i<=a;i++) {
+		 sum=sum+i;
+		
+			
+		}
+		System.out.println(sum);
+	}
+
+}
